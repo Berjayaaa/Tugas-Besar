7de50fcfeec705542a06b39ba2b87d23a481de7a
fadilramadhan <fadil_raymondy@yahoo.co.id>
2015-04-26 17:46:23 +0700

    Update RuangKelas.java
    Edit Class RuangKelas

--- TestKelas/src/testkelas/RuangKelas.java
+++ TestKelas/src/testkelas/RuangKelas.java
@@ -273,331 +273,191 @@ public class RuangKelas {
         return banwidth;
     }
 
-    /**
-     * @param banwidth the banwidth to set
-     */
     public void setBanwidth(String banwidth) {
         this.banwidth = banwidth;
     }
 
-    /**
-     * @return the jmCCTV
-     */
     public int getJmCCTV() {
         return jmCCTV;
     }
 
-    /**
-     * @param jmCCTV the jmCCTV to set
-     */
     public void setJmCCTV(int jmCCTV) {
         this.jmCCTV = jmCCTV;
     }
 
-    /**
-     * @return the konCCTV
-     */
     public int getKonCCTV() {
         return konCCTV;
     }
 
-    /**
-     * @param konCCTV the konCCTV to set
-     */
     public void setKonCCTV(int konCCTV) {
         this.konCCTV = konCCTV;
     }
 
-    /**
-     * @return the posCCTV
-     */
     public String getPosCCTV() {
         return posCCTV;
     }
 
-    /**
-     * @param posCCTV the posCCTV to set
-     */
     public void setPosCCTV(String posCCTV) {
         this.posCCTV = posCCTV;
     }
 
-    /**
-     * @return the konLantai
-     */
     public String getKonLantai() {
         return konLantai;
     }
 
-    /**
-     * @param konLantai the konLantai to set
-     */
     public void setKonLantai(String konLantai) {
         this.konLantai = konLantai;
     }
 
-    /**
-     * @return the konDinding
-     */
     public String getKonDinding() {
         return konDinding;
     }
 
-    /**
-     * @param konDinding the konDinding to set
-     */
     public void setKonDinding(String konDinding) {
         this.konDinding = konDinding;
     }
 
-    /**
-     * @return the konAtap
-     */
     public String getKonAtap() {
         return konAtap;
     }
 
-    /**
-     * @param konAtap the konAtap to set
-     */
     public void setKonAtap(String konAtap) {
         this.konAtap = konAtap;
     }
 
-    /**
-     * @return the konPintu
-     */
     public String getKonPintu() {
         return konPintu;
     }
 
-    /**
-     * @param konPintu the konPintu to set
-     */
     public void setKonPintu(String konPintu) {
         this.konPintu = konPintu;
     }
 
-    /**
-     * @return the konJendela
-     */
     public String getKonJendela() {
         return konJendela;
     }
 
-    /**
-     * @param konJendela the konJendela to set
-     */
     public void setKonJendela(String konJendela) {
         this.konJendela = konJendela;
     }
 
-    /**
-     * @return the sirkulasiUdara
-     */
     public String getSirkulasiUdara() {
         return sirkulasiUdara;
     }
 
-    /**
-     * @param sirkulasiUdara the sirkulasiUdara to set
-     */
     public void setSirkulasiUdara(String sirkulasiUdara) {
         this.sirkulasiUdara = sirkulasiUdara;
     }
 
-    /**
-     * @return the pencahayaan
-     */
     public double getPencahayaan() {
         return pencahayaan;
     }
 
-    /**
-     * @param pencahayaan the pencahayaan to set
-     */
     public void setPencahayaan(double pencahayaan) {
         this.pencahayaan = pencahayaan;
     }
 
-    /**
-     * @return the kelembapan
-     */
     public double getKelembapan() {
         return kelembapan;
     }
 
-    /**
-     * @param kelembapan the kelembapan to set
-     */
     public void setKelembapan(double kelembapan) {
         this.kelembapan = kelembapan;
     }
 
-    /**
-     * @return the suhu
-     */
     public double getSuhu() {
         return suhu;
     }
 
-    /**
-     * @param suhu the suhu to set
-     */
     public void setSuhu(double suhu) {
         this.suhu = suhu;
     }
 
-    /**
-     * @return the kebisingan
-     */
     public String getKebisingan() {
         return kebisingan;
     }
-
-    /**
-     * @param kebisingan the kebisingan to set
-     */
+    
     public void setKebisingan(String kebisingan) {
         this.kebisingan = kebisingan;
     }
 
-    /**
-     * @return the bau
-     */
     public String getBau() {
         return bau;
     }
 
-    /**
-     * @param bau the bau to set
-     */
     public void setBau(String bau) {
         this.bau = bau;
     }
 
-    /**
-     * @return the kebocoran
-     */
     public String getKebocoran() {
         return kebocoran;
     }
 
-    /**
-     * @param kebocoran the kebocoran to set
-     */
     public void setKebocoran(String kebocoran) {
         this.kebocoran = kebocoran;
     }
 
-    /**
-     * @return the kerusakan
-     */
     public String getKerusakan() {
         return kerusakan;
     }
 
-    /**
-     * @param kerusakan the kerusakan to set
-     */
     public void setKerusakan(String kerusakan) {
         this.kerusakan = kerusakan;
     }
 
-    /**
-     * @return the keausan
-     */
     public String getKeausan() {
         return keausan;
     }
 
-    /**
-     * @param keausan the keausan to set
-     */
     public void setKeausan(String keausan) {
         this.keausan = keausan;
     }
 
-    /**
-     * @return the kekokohan
-     */
+    
     public String getKekokohan() {
         return kekokohan;
     }
 
-    /**
-     * @param kekokohan the kekokohan to set
-     */
     public void setKekokohan(String kekokohan) {
         this.kekokohan = kekokohan;
     }
 
-    /**
-     * @return the kunciPintu
-     */
     public String getKunciPintu() {
         return kunciPintu;
     }
 
-    /**
-     * @param kunciPintu the kunciPintu to set
-     */
     public void setKunciPintu(String kunciPintu) {
         this.kunciPintu = kunciPintu;
     }
 
-    /**
-     * @return the kunciJendela
-     */
     public String getKunciJendela() {
         return kunciJendela;
     }
 
-    /**
-     * @param kunciJendela the kunciJendela to set
-     */
     public void setKunciJendela(String kunciJendela) {
         this.kunciJendela = kunciJendela;
     }
 
-    /**
-     * @return the bahaya
-     */
     public String getBahaya() {
         return bahaya;
     }
 
-    /**
-     * @param bahaya the bahaya to set
-     */
     public void setBahaya(String bahaya) {
         this.bahaya = bahaya;
     }
 
-    /**
-     * @return the studi
-     */
     public String getStudi() {
         return studi;
     }
 
-    /**
-     * @param studi the studi to set
-     */
     public void setStudi(String studi) {
         this.studi = studi;
     }
 
-    /**
-     * @return the posStopKontak
-     */
     public String getPosStopKontak() {
         return posStopKontak;
     }
 
-    /**
-     * @param posStopKontak the posStopKontak to set
-     */
     public void setPosStopKontak(String posStopKontak) {
         this.posStopKontak = posStopKontak;
     }
