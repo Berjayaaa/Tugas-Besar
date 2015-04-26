4c9fb8228cbe28523b99fccec3d6fe71091517f4
Berjayaaa <inijaya24@gmail.com>
2015-04-26 22:12:58 +0700

    Update ControlKelas.java

--- TestKelas/src/testkelas/ControlKelas.java
+++ TestKelas/src/testkelas/ControlKelas.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package testkelas;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Berjayaaa
- */
+
 public class ControlKelas {
 
     Scanner in = new Scanner(System.in);
