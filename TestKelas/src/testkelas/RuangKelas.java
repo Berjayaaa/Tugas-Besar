cc254b68d198ae09d201159bcf53020194058721
fadilramadhan <fadil_raymondy@yahoo.co.id>
2015-04-26 16:48:47 +0700

    Update RuangKelas.java
    Edit Class RuangKelas

--- TestKelas/src/testkelas/RuangKelas.java
+++ TestKelas/src/testkelas/RuangKelas.java
@@ -1,15 +1,5 @@
 package testkelas;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Berjayaaa
- */
 public class RuangKelas {
 
     private String nama;
