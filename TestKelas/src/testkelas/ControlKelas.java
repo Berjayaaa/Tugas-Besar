83886f125ef865e2a2ae111244763e8f08b0483b
Berjayaaa <inijaya24@gmail.com>
2015-04-26 10:50:23 +0700

    buat 3 kelas

---
+++ TestKelas/src/testkelas/ControlKelas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testkelas;
+
+/**
+ *
+ * @author Berjayaaa
+ */
+public class ControlKelas {
+    
+}
