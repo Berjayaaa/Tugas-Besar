76ec77fe8da336e57a6ed97db070ec254dd6e97f
fadilramadhan <fadil_raymondy@yahoo.co.id>
2015-05-17 23:27:45 +0700

    New Update Kenyamanan.java

--- TestKelas/src/testkelas/Kenyamanan.java
+++ TestKelas/src/testkelas/Kenyamanan.java
@@ -3,8 +3,9 @@ package testkelas;
 
 
 public class Kenyamanan extends ControlKelas{
+    
     @Override
-    void input20(){
+    void inputKebisingan(){
         System.out.print("Masukkan kebisingan (1. bising\n2. tidak bising): ");
         super.setKebisingan(in.nextInt());
     }
@@ -21,7 +22,7 @@ public class Kenyamanan extends ControlKelas{
     }
     
     @Override
-    void input21(){
+    void inputBau(){
         
         System.out.print("Masukkan bau (1. bau\n2.  tidak bau): ");
         super.setBau(in.nextInt());
@@ -40,7 +41,7 @@ public class Kenyamanan extends ControlKelas{
     
     
     @Override
-    void input22(){
+    void inputKebocoran(){
         System.out.print("Masukkan kebocoran (1. bocor\n2. tidak bocor): ");
         super.setKebocoran(in.nextInt());
     }
@@ -57,7 +58,7 @@ public class Kenyamanan extends ControlKelas{
     }
     
     @Override
-    void input23(){
+    void inputKerusakan(){
         System.out.print("Masukkan kerusakan (1. rusak\n2. tidak rusak): ");
         super.setKerusakan(in.nextInt());
     }
@@ -74,7 +75,7 @@ public class Kenyamanan extends ControlKelas{
     }
     
     @Override
-    void input24(){
+    void inputKeausan(){
         System.out.print("Masukkan keausan (1. aus\n2. tidak aus): ");
         super.setKeausan(in.nextInt());
     }
@@ -83,13 +84,14 @@ public class Kenyamanan extends ControlKelas{
     String analisisKeausan(){
         if(super.getKeausan()==2){
             totSesuai++;
-            return ("Keausan: SESUIA");
+            return ("Keausan: SESUIA\n\n");
         }
         else{
-            return ("Keausan: TIDAK SESUIA");}
+            return ("Keausan: TIDAK SESUIA\n\n");}
     }
     @Override
     void cetakKenyamanan(){
+        System.out.println("---HASIL ANALISIS KENYAMANAN---");
         /*System.out.println("Kebisingan: "+super.getKebisingan());
         System.out.println("Bau: "+super.getBau());
         System.out.println("Kebocoran: "+super.getKebocoran());
